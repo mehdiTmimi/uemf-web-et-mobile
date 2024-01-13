88e0fe610228881b5bdc83fefd02e02163203977
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-01-13 11:32:28 +0100

    ok

---
+++ webservice/main.js
@@ -0,0 +1,11 @@
+/*const os = require("os")
+console.log(os.platform());*/
+// const fs = require("fs") // importation
+// fs.writeFile("./data.txt","salut uemf 2024",()=>{
+//     console.log("end writing")
+// })
+// console.log("fin")
+const {name,age,f1} = require("./utils.js")
+// 1 - executer le ficheir utils
+// 2 - retourne le contenu exports a la fin
+console.log(name,age)
---
+++ webservice/utils.js
@@ -0,0 +1,11 @@
+// tout fichier avec extension js est un module
+console.log("execution du utils .js")
+module.exports={
+    name:"mehdi",
+    age:31,
+    f1:()=>{
+        
+    }
+}
+
+console.log("fiiiin execution du utils .js")
